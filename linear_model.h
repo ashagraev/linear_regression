0dc0c080daecab279fd90be6aeef515719e3926a
ashagraev <alex-shagraev@ya.ru>
2016-02-13 17:11:23 +0300

    save & load for linear models

--- linear_model.h
+++ linear_model.h
@@ -2,6 +2,9 @@
 
 #include <vector>
 #include <numeric>
+#include <string>
+
+#include <fstream>
 
 using namespace std;
 
@@ -18,5 +21,34 @@ struct TLinearModel {
     template <typename T>
     double Prediction(const vector<T>& features) const {
         return inner_product(Coefficients.begin(), Coefficients.end(), features.begin(), Intercept);
+    }
+
+    void SaveToFile(const string& modelPath) {
+        ofstream modelOut(modelPath);
+        modelOut.precision(20);
+
+        modelOut << Coefficients.size() << " ";
+        modelOut << Intercept << " ";
+
+        for (const double coefficient : Coefficients) {
+            modelOut << coefficient << " ";
+        }
+    }
+
+    static TLinearModel LoadFromFile(const string& modelPath) {
+        ifstream modelIn(modelPath);
+
+        size_t featuresCount;
+        modelIn >> featuresCount;
+
+        TLinearModel model;
+        model.Coefficients.resize(featuresCount);
+
+        modelIn >> model.Intercept;
+        for (size_t featureIdx = 0; featureIdx < featuresCount; ++featureIdx) {
+            modelIn >> model.Coefficients[featureIdx];
+        }
+
+        return model;
     }
 };
