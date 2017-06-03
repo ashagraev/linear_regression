7ea541898380bd28472e2b818ff14abcc9d35a5e
ashagraev <alex-shagraev@ya.ru>
2017-06-03 10:45:01 +0300

    moved implementations to a source file

---
+++ src/lib/linear_model.cpp
@@ -0,0 +1,36 @@
+#include "linear_model.h"
+
+TLinearModel::TLinearModel(size_t featuresCount /*= 0*/)
+    : Coefficients(featuresCount)
+    , Intercept(0.)
+{
+}
+
+void TLinearModel::SaveToFile(const string& modelPath) {
+    ofstream modelOut(modelPath);
+    modelOut.precision(20);
+
+    modelOut << Coefficients.size() << " ";
+    modelOut << Intercept << " ";
+
+    for (const double coefficient : Coefficients) {
+        modelOut << coefficient << " ";
+    }
+}
+
+TLinearModel TLinearModel::LoadFromFile(const string& modelPath) {
+    ifstream modelIn(modelPath);
+
+    size_t featuresCount;
+    modelIn >> featuresCount;
+
+    TLinearModel model;
+    model.Coefficients.resize(featuresCount);
+
+    modelIn >> model.Intercept;
+    for (size_t featureIdx = 0; featureIdx < featuresCount; ++featureIdx) {
+        modelIn >> model.Coefficients[featureIdx];
+    }
+
+    return model;
+}
--- src/lib/linear_model.h
+++ src/lib/linear_model.h
@@ -12,43 +12,13 @@ struct TLinearModel {
     vector<double> Coefficients;
     double Intercept;
 
-    explicit TLinearModel(size_t featuresCount = 0)
-        : Coefficients(featuresCount)
-        , Intercept(0.)
-    {
-    }
+    explicit TLinearModel(size_t featuresCount = 0);
+
+    void SaveToFile(const string& modelPath);
+    static TLinearModel LoadFromFile(const string& modelPath);
 
     template <typename T>
     double Prediction(const vector<T>& features) const {
         return inner_product(Coefficients.begin(), Coefficients.end(), features.begin(), Intercept);
-    }
-
-    void SaveToFile(const string& modelPath) {
-        ofstream modelOut(modelPath);
-        modelOut.precision(20);
-
-        modelOut << Coefficients.size() << " ";
-        modelOut << Intercept << " ";
-
-        for (const double coefficient : Coefficients) {
-            modelOut << coefficient << " ";
-        }
-    }
-
-    static TLinearModel LoadFromFile(const string& modelPath) {
-        ifstream modelIn(modelPath);
-
-        size_t featuresCount;
-        modelIn >> featuresCount;
-
-        TLinearModel model;
-        model.Coefficients.resize(featuresCount);
-
-        modelIn >> model.Intercept;
-        for (size_t featureIdx = 0; featureIdx < featuresCount; ++featureIdx) {
-            modelIn >> model.Coefficients[featureIdx];
-        }
-
-        return model;
     }
 };
