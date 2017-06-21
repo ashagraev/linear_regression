850af5357dd7637347251b31b47f4b180da6a5cd
Aleksey Shagraev <alex-shagraev@ya.ru>
2017-06-21 15:09:55 +0300

    convert to vw mode added

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -22,7 +22,9 @@ struct TRunData {
         runData.Mode = argv[1];
 
         runData.FeaturesFilePath = argv[2];
-        runData.ModelFilePath = argv[3];
+        if (argc > 3) {
+            runData.ModelFilePath = argv[3];
+        }
 
         if (argc > 4) {
             runData.LearningMode = argv[4];
@@ -49,6 +51,7 @@ int PrintHelp() {
     cerr << "    linear_regression learn features_path model_path learning_method" << endl;
     cerr << "    linear_regression predict features_path model_path" << endl;
     cerr << "    linear_regression injure-pool features_path injure_factor injure_offset" << endl;
+    cerr << "    linear_regression to-vowpal-wabbit features_path" << endl;
     cerr << "available learn modes:" << endl;
     cerr << "    fast_bslr for simple linear regression" << endl;
     cerr << "    kahan_bslr for simple linear regression with Kahan's summator" << endl;
@@ -112,6 +115,13 @@ int DoInjurePool(const TRunData &runData) {
     return 0;
 }
 
+int ToVowpalWabbit(const TRunData &runData) {
+    TPool pool;
+    pool.ReadFromFeatures(runData.FeaturesFilePath);
+    pool.PrintForVowpalWabbit(cout);
+    return 0;
+}
+
 int main(int argc, const char** argv) {
     if (!TRunData::ParametersAreCorrect(argc, argv)) {
         return PrintHelp();
@@ -127,6 +137,9 @@ int main(int argc, const char** argv) {
     }
     if (runData.Mode == "injure-pool") {
         return DoInjurePool(runData);
+    }
+    if (runData.Mode == "to-vowpal-wabbit") {
+        return ToVowpalWabbit(runData);
     }
 
     return PrintHelp();
