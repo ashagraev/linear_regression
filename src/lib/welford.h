aefe91bffacb79b36d88bfc32f5b077aada11cf8
Aleksey Shagraev <alex-shagraev@ya.ru>
2017-06-21 16:16:52 +0300

    removed comments

--- src/lib/welford.h
+++ src/lib/welford.h
@@ -2,7 +2,6 @@
 
 #include "kahan.h"
 
-// accurately computes (w_1 * x_1 + w_2 * x_2 + ... + w_n * x_n) / (w_1 + w_2 + ... + w_n)
 class TMeanCalculator {
 private:
     double Mean = 0.;
@@ -13,7 +12,6 @@ public:
     double GetSumWeights() const;
 };
 
-// accurately computes (w_1 * x_1 * x_1 + w_2 * x_2 * x_2 + ... + w_n * x_n * x_n) / (w_1 + w_2 + ... + w_n)
 class TDeviationCalculator {
 private:
     double Deviation = 0.;
