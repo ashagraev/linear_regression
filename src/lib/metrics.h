f52c3d7f75a60d9cc974c746fe3c4080f656e71b
ashagraev <alex-shagraev@ya.ru>
2017-06-25 00:05:41 +0300

    determination coefficient calculation added

--- src/lib/metrics.h
+++ src/lib/metrics.h
@@ -4,19 +4,29 @@
 
 class TRegressionMetricsCalculator {
 private:
-    TDeviationCalculator VarianceCalculator;
+    TVarianceCalculator VarianceCalculator;
     TMeanCalculator MSECalculator;
 public:
     void Add(const double prediction, const double target, const double weight);
     double RMSE() const;
+    double DeterminationCoefficient() const;
 };
 
 template <typename TModel, typename TIterator>
-double RMSE(TIterator iterator, const TModel& model) {
+double Metric(TIterator iterator, const TModel& model, double (TRegressionMetricsCalculator::*func)() const) {
     TRegressionMetricsCalculator rmc;
     for (; iterator.IsValid(); ++iterator) {
         rmc.Add(model.Prediction(*iterator), iterator->Goal, iterator->Weight);
     }
-    return rmc.RMSE();
+    return (rmc.*func)();
 }
 
+template <typename TModel, typename TIterator>
+double RMSE(TIterator iterator, const TModel& model) {
+    return Metric(iterator, model, &TRegressionMetricsCalculator::RMSE);
+}
+
+template <typename TModel, typename TIterator>
+double DeterminationCoefficient(TIterator iterator, const TModel& model) {
+    return Metric(iterator, model, &TRegressionMetricsCalculator::DeterminationCoefficient);
+}
