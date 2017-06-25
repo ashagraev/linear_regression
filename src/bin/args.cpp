82639171c764440680df042acda293b00c36fe2c
ashagraev <alex-shagraev@ya.ru>
2017-06-25 09:42:09 +0300

    remove that bad exception-based code

--- src/bin/args.cpp
+++ src/bin/args.cpp
@@ -6,57 +6,53 @@
 #include <unordered_set>
 
 void TArgsParser::DoParse(int argc, const char** argv) const {
-    try {
-        std::unordered_set<std::string> usedKeys;
-
-        while (argc) {
-            std::string argument = argv[0];
-
-            auto parser = Parsers.find(argument);
-            if (parser == Parsers.end()) {
-                std::string message = "unknown argument: " + argument;
-                throw std::exception(message.c_str());
-            }
-
-            if (argc < 2) {
-                std::string message = "missing parameter for " + argument;
-                throw std::exception(message.c_str());
-            }
-
-            usedKeys.insert(argument);
-            parser->second->SetValue(argv[1]);
-
-            argc -= 2;
-            argv += 2;
-        }
-
-        std::vector<std::string> lostArguments;
-        for (auto&& keyWithParser : Parsers) {
-            if (!keyWithParser.second->IsRequired()) {
-                continue;
-            }
-
-            const std::string& key = keyWithParser.first;
-            if (usedKeys.find(key) == usedKeys.end()) {
-                lostArguments.push_back(key);
-            }
-        }
-
-        if (!lostArguments.empty()) {
-            std::string message = "those arguments are required: ";
-            message += lostArguments.front();
-            for (size_t i = 1; i < lostArguments.size(); ++i) {
-                message += ", " + lostArguments[i];
-            }
-            throw std::exception(message.c_str());
-        }
-    } catch (std::exception ex) {
-        std::cerr << ex.what();
-        std::cerr << std::endl;
-        PrintHelp();
-
+    auto callError = [this](const std::string& message) {
+        std::cerr << message << std::endl;
+        this->PrintHelp();
         exit(1);
-    } 
+    };
+
+    std::unordered_set<std::string> usedKeys;
+
+    while (argc) {
+        std::string argument = argv[0];
+
+        auto parser = Parsers.find(argument);
+        if (parser == Parsers.end()) {
+            callError("unknown argument: " + argument);
+        }
+
+        if (argc < 2) {
+            callError("missing parameter for " + argument);
+        }
+
+        usedKeys.insert(argument);
+        parser->second->SetValue(argv[1]);
+
+        argc -= 2;
+        argv += 2;
+    }
+
+    std::vector<std::string> lostArguments;
+    for (auto&& keyWithParser : Parsers) {
+        if (!keyWithParser.second->IsRequired()) {
+            continue;
+        }
+
+        const std::string& key = keyWithParser.first;
+        if (usedKeys.find(key) == usedKeys.end()) {
+            lostArguments.push_back(key);
+        }
+    }
+
+    if (!lostArguments.empty()) {
+        std::string message = "those arguments are required: ";
+        message += lostArguments.front();
+        for (size_t i = 1; i < lostArguments.size(); ++i) {
+            message += ", " + lostArguments[i];
+        }
+        callError(message);
+    }
 }
 
 void TArgsParser::PrintHelp() const {
