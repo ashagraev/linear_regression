772b1e77f1541d0ba2947f34a8001251e9ee3f99
Aleksey Shagraev <alex-shagraev@ya.ru>
2017-06-30 10:55:28 +0300

    roll back some accidental changes =O

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -122,11 +122,77 @@ int DoPredict(int argc, const char** argv) {
     return 0;
 }
 
+struct TCrossValidationResult {
+    double MeanDeterminationCoefficient;
+    double LearningTimeInSeconds;
+};
+
+TCrossValidationResult CrossValidation(
+    const TPool& pool,
+    const size_t foldsCount,
+    const size_t runsCount,
+    const std::string& learningMode,
+    const std::string verboseMode, bool verbose)
+{
+    double learningTime = 0;
+
+    TPool::TCVIterator learnIterator = pool.LearnIterator(foldsCount);
+    TPool::TCVIterator testIterator = pool.TestIterator(foldsCount);
+
+    TMeanCalculator meanDeterminationCoefficientCalculator;
+    for (size_t runIdx = 0; runIdx < runsCount; ++runIdx) {
+        learnIterator.ResetShuffle();
+        testIterator.ResetShuffle();
+
+        TMeanCalculator meanFoldDeterminationCoefficientCalculator;
+        for (size_t fold = 0; fold < foldsCount; ++fold) {
+            learnIterator.SetTestFold(fold);
+            testIterator.SetTestFold(fold);
+
+            TLinearModel linearModel;
+            {
+                TTimer timer;
+                linearModel = Solve(learnIterator, learningMode);
+                learningTime += timer.GetSecondsPassed();
+            }
+            const double determinationCoefficient = TRegressionMetricsCalculator::Build(testIterator, linearModel).DeterminationCoefficient();
+
+            if (verbose && verboseMode == "folds") {
+                std::cout << "    ";
+                if (runsCount > 1) {
+                    std::cout << "    run #" << runIdx << ", ";
+                }
+                std::cout << "fold #" << fold << ": R^2 = " << determinationCoefficient << std::endl;
+            }
+
+            meanFoldDeterminationCoefficientCalculator.Add(determinationCoefficient);
+        }
+
+        if (verbose && verboseMode != "overall") {
+            if (runsCount > 1) {
+                std::cout << "    run #" << runIdx << ", ";
+            }
+            std::cout << "CV R^2: " << meanFoldDeterminationCoefficientCalculator.GetMean() << std::endl;
+        }
+
+        meanDeterminationCoefficientCalculator.Add(meanFoldDeterminationCoefficientCalculator.GetMean());
+    }
+
+    if (verbose && runsCount > 1) {
+        std::cout << "CV RMSE over " << runsCount << " runs: " << meanDeterminationCoefficientCalculator.GetMean() << std::endl;
+    }
+
+    return {meanDeterminationCoefficientCalculator.GetMean(), learningTime};
+}
+
 int DoCrossValidation(int argc, const char** argv) {
     std::string featuresPath;
 
     std::string learningMode = "welford_lr";
     size_t foldsCount = 5;
+    size_t runsCount = 1;
+
+    std::string verboseMode = "folds";
 
     {
         TArgsParser argsParser;
@@ -134,6 +200,9 @@ int DoCrossValidation(int argc, const char** argv) {
         argsParser.AddHandler("method", &learningMode, "learning mode, one from: fast_bslr, kahan_bslr, welford_bslr, fast_lr, welford_lr, precise_welford_lr").Optional();
 
         argsParser.AddHandler("folds", &foldsCount, "cross-validation folds count").Optional();
+        argsParser.AddHandler("runs", &runsCount, "cross-validation runs count").Optional();
+
+        argsParser.AddHandler("verbose", &verboseMode, "verbose mode, one of: folds, cv, overall").Optional();
 
         argsParser.DoParse(argc, argv);
     }
@@ -144,26 +213,97 @@ int DoCrossValidation(int argc, const char** argv) {
         pool.ReadFromFeatures(featuresPath);
     }
 
-    TPool::TCVIterator learnIterator = pool.LearnIterator(foldsCount);
-    TPool::TCVIterator testIterator = pool.TestIterator(foldsCount);
+    CrossValidation(pool, foldsCount, runsCount, learningMode, verboseMode, true);
+
+    return 0;
+}
+
+int DoResearchMethods(int argc, const char** argv) {
+    std::string featuresPath;
+
+    size_t foldsCount = 5;
+    size_t runsCount = 1;
+
+    size_t tasksCount = 5;
+    double degradeFactor = 0.2;
 
     {
-        TTimer timer("cross validation taken");
+        TArgsParser argsParser;
+        argsParser.AddHandler("features", &featuresPath, "features file path").Required();
 
-        TMeanCalculator meanDCCalculator;
-        for (size_t fold = 0; fold < foldsCount; ++fold) {
-            learnIterator.SetTestFold(fold);
-            testIterator.SetTestFold(fold);
+        argsParser.AddHandler("tasks", &tasksCount, "number of research tasks").Optional();
+        argsParser.AddHandler("degrade", &degradeFactor, "task-to-task degrade level").Optional();
 
-            const TLinearModel linearModel = Solve(learnIterator, learningMode);
-            const double determinationCoefficient = TRegressionMetricsCalculator::Build(testIterator, linearModel).DeterminationCoefficient();
+        argsParser.AddHandler("folds", &foldsCount, "cross-validation folds count").Optional();
+        argsParser.AddHandler("runs", &runsCount, "cross-validation runs count").Optional();
 
-            std::cout << "fold #" << fold << ": R^2 = " << determinationCoefficient << std::endl;
+        argsParser.DoParse(argc, argv);
+    }
 
-            meanDCCalculator.Add(determinationCoefficient);
+    TPool pool;
+    pool.ReadFromFeatures(featuresPath);
+
+    std::vector<std::pair<double, double>> injureFactorsAndOffsets;
+    {
+        double injureFactor = 1.;
+        double injureOffset = 1.;
+
+        for (size_t taskIdx = 0; taskIdx < tasksCount; ++taskIdx) {
+            injureFactorsAndOffsets.push_back(std::make_pair(injureFactor, injureOffset));
+
+            injureFactor *= degradeFactor;
+            injureOffset /= degradeFactor;
         }
+    }
 
-        std::cout << "CV R^2: " << meanDCCalculator.GetMean() << std::endl;
+    const std::vector<std::string> learningModes = { "fast_bslr", "kahan_bslr", "welford_bslr", "fast_lr", "welford_lr", "precise_welford_lr" };
+    std::vector<std::vector<double>> scores(learningModes.size());
+    std::vector<double> fullLearnTime(learningModes.size());
+
+    for (const std::pair<double, double>& injureFactorAndOffset : injureFactorsAndOffsets) {
+        const double injureFactor = injureFactorAndOffset.first;
+        const double injureOffset = injureFactorAndOffset.second;
+
+        const TPool injuredPool = pool.InjuredPool(injureFactor, injureOffset);
+
+        std::cerr << "injure factor: " << injureFactor << std::endl;
+        std::cerr << "injure offset: " << injureOffset << std::endl;
+
+        for (size_t methodIdx = 0; methodIdx < learningModes.size(); ++methodIdx) {
+            const TCrossValidationResult cvResult = CrossValidation(injuredPool, foldsCount, runsCount, learningModes[methodIdx], "", false);
+
+            std::stringstream ss;
+            ss << "   ";
+            ss << learningModes[methodIdx];
+            while (ss.str().size() < 25) {
+                ss << " ";
+            }
+
+            ss.precision(5);
+
+            ss << "time: " << cvResult.LearningTimeInSeconds << "    " << "R^2: " << cvResult.MeanDeterminationCoefficient;
+
+            std::cerr << ss.str() << std::endl;
+
+            scores[methodIdx].push_back(cvResult.MeanDeterminationCoefficient);
+            fullLearnTime[methodIdx] += cvResult.LearningTimeInSeconds;
+        }
+        std::cerr << std::endl;
+    }
+
+    std::cerr << "full learning time:" << std::endl;
+    for (size_t methodIdx = 0; methodIdx < learningModes.size(); ++methodIdx) {
+        std::stringstream ss;
+        ss << "   ";
+        ss << learningModes[methodIdx];
+        while (ss.str().size() < 25) {
+            ss << " ";
+        }
+        ss.precision(5);
+
+        ss << fullLearnTime[methodIdx] << "s";
+
+        std::cerr << ss.str() << std::endl;
     }
 
     return 0;
@@ -171,8 +311,8 @@ int DoCrossValidation(int argc, const char** argv) {
 
 int DoInjurePool(int argc, const char** argv) {
     std::string featuresPath;
-    double injureFactor = 1000;
-    double injureOffset = 1000;
+    double injureFactor = 1e-3;
+    double injureOffset = 1e+3;
 
     {
         TArgsParser argsParser;
@@ -184,7 +324,7 @@ int DoInjurePool(int argc, const char** argv) {
 
     TPool pool;
     pool.ReadFromFeatures(featuresPath);
-    pool.InjurePool(injureFactor, injureOffset);
+    pool = pool.InjuredPool(injureFactor, injureOffset);
     pool.PrintForFeatures(std::cout);
     return 0;
 }
@@ -223,6 +363,7 @@ int main(int argc, const char** argv) {
     modeChooser.Add("learn", &DoLearn, "learn model from features");
     modeChooser.Add("predict", &DoPredict, "apply learned model to features");
     modeChooser.Add("cv", &DoCrossValidation, "run cross-validation check");
+    modeChooser.Add("research", &DoResearchMethods, "research learning methods on set of injured pools");
     modeChooser.Add("injure-pool", &DoInjurePool, "create injured pool from source features");
     modeChooser.Add("to-vowpal-wabbit", &ToVowpalWabbit, "create VowpalWabbit-compatible pool");
     modeChooser.Add("to-svm-light", &ToSVMLight, "create SVMLight-compatible pool");
